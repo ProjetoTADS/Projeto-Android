369dacc7142e91c25976309bc91952628a2a24e5
Anderson F Lima <andersonfaustinolima@gmail.com>
2017-11-05 23:59:31 -0200

    ListView
    Up da lista

--- projetoAndroid/app/src/main/java/com/example/crowwinter/projetoandroid/Listar_todos.java
+++ projetoAndroid/app/src/main/java/com/example/crowwinter/projetoandroid/Listar_todos.java
@@ -2,12 +2,43 @@ package com.example.crowwinter.projetoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Listar_todos extends AppCompatActivity {
+
+    private ListView listaitens;
+    private String[] itens = {
+            "Anderson F Lima","915125292","Uninove E '10'"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_todos);
+
+
+        listaitens = (ListView) findViewById(R.id.listViewID);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        listaitens.setAdapter(adapter);
+
+        listaitens.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int codigoPosicao = i;
+                String valorClicado = (String) listaitens.getItemAtPosition(codigoPosicao);
+                Toast.makeText(getApplicationContext(),valorClicado,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
